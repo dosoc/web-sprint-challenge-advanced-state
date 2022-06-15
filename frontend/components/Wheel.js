4f0efe2f0eac58db466f427359b4c08a0c5d6c0b
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-06-15 17:14:30 -0500

    removed unused lines of code

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -14,12 +14,6 @@ function Wheel(props) {
         {wheelArr.map((cog, idx)=>{
           return <div key={idx} className={idx===props.wheel?"cog active":"cog"} style={{ "--i": idx }}>{idx===props.wheel?"B":cog}</div>
         })}
-        {/* <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>--i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
         <button id="counterClockwiseBtn" onClick={()=>handleClick("up")} >Counter clockwise</button>
